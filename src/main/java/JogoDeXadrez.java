a63264f466aa91307dea82acaa0eeeac0d2d8b45
Bianca Noronha <teixeiranoronhabianca@gmail.com>
2026-06-27 23:12:31 -0300

    Alteração JogoDeXadrez-Atividade 06

--- src/main/java/JogoDeXadrez.java
+++ src/main/java/JogoDeXadrez.java
@@ -3,14 +3,7 @@ class JogoDeXadrez implements Jogo
     public void iniciar()
     {
         Tabuleiro tabuleiro = new Tabuleiro();
-        Jogador jogadorHumano = new JogadorHumano('b');
-        Jogador jogadorSintetico = new JogadorSintetico('p');
-
-        while (!tabuleiro.acabouOJogo()) {
-            jogadorHumano.jogar(tabuleiro, "", 0);
-            jogadorSintetico.jogar(tabuleiro, "", 0);
-            break;
-        }
+        tabuleiro.iniciarPartida();
     }
 
     public static void main(String[] args)
